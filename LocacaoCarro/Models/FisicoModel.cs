46df8bc963abad05ef3f4d9169fcf5888ef7cd62
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-10-18 16:04:38 -0300

    :sparkles: feat: Adicionando projeto de Locação de Veículos

---
+++ LocacaoCarro/Models/FisicoModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocacaoCarro.Models
+{
+	public class FisicoModel : ClienteModel
+	{
+		[Key]
+		public int codigo { get; set; }
+		public string cpf { get; set; }
+		public string rg { get; set; }
+	}
+}
